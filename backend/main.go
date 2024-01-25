05e13334c21c0e0b284b765aaa3ed20fed41d5d4
Tasnim Zotder <tasnimzotder@gmail.com>
2024-01-25 08:34:02 +0530

    add cd - be

--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/spf13/viper"
 	"github.com/tasnimzotder/x-tracker/api"
 	db "github.com/tasnimzotder/x-tracker/db/sqlc"
 	"github.com/tasnimzotder/x-tracker/utils"
@@ -14,10 +15,12 @@ import (
 func main() {
 	var err error
 
-	config, err := utils.LoadConfig(".")
+	_, err = utils.LoadConfig(".")
 	if err != nil {
 		log.Fatal("cannot load config:", err)
 	}
+
+	println(viper.GetString("SERVER_ADDRESS"))
 
 	//	aws session
 	aws_session, err := session.NewSession(&aws.Config{
@@ -27,7 +30,7 @@ func main() {
 		log.Fatalf("failed to create aws session: %v", err)
 	}
 
-	connPool, err := pgxpool.New(context.Background(), config.DBSource)
+	connPool, err := pgxpool.New(context.Background(), viper.GetString("DB_SOURCE"))
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
 	}
@@ -35,7 +38,7 @@ func main() {
 	queries := db.New(connPool)
 	server := api.NewServer(aws_session, queries)
 
-	err = server.Start(config.ServerAddress)
+	err = server.Start(viper.GetString("SERVER_ADDRESS"))
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
